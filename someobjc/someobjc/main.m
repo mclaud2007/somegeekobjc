bc5f578e2753f016d65cafc2e9c204123ee61795
mclaud2007 <grigoriy@martushin.ru>
2020-05-07 14:10:52 +0300

    1. Программа предлагает ввести неограниченное число строчек текста (пока не будет введено -1), которые сохраняются в массив. Далее этот массив записывается в UsersInput.plist в директорию "Документы". После чего массив очищается, а затем повторно загружается из файла. В рамках данной работы заодно  узнал как считывать строки на objective-c (ранее были с этим трудности в одном из заданий). Т.к. приложение я выбрал консольное для мака файл создастся в директории "Документы" текущего пользователя.
    2. В процессе применял расширенный вариант брейкпойнта. Очень удобная особенность, о которой я ранее не знал: возможность распечатать переменную и автоматически запустить приложение на выполнение далее.

--- someobjc/someobjc/main.m
+++ someobjc/someobjc/main.m
@@ -2,72 +2,55 @@
 //  main.m
 //  someobjc
 //
-//  Created by Григорий Мартюшин on 12.04.2020.
+//  Created by Григорий Мартюшин on 07.05.2020.
 //  Copyright © 2020 Григорий Мартюшин. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
-#import "Calculator.h"
-#import "main.h"
-
-void selectCalculatorOperation (Calculator *calc) {
-    int menu_item = 0;
-    
-    // Выводим приветствие
-    printf("\nSelect what did you want to do with entered numbers.");
-    printf("\n--------\n");
-    printf(" 1: First plus second\n");
-    printf(" 2: First minus scond\n");
-    printf(" 3: First multiply second\n");
-    printf(" 4: Second divided by first\n");
-    printf("-1: Exit\n");
-    printf("--------\n");
-    
-    printf("Your chois: ");
-    // Просим выбрать элемент меню
-    scanf("%int", &menu_item);
-    
-    
-    // Проверим введенное число
-    switch (menu_item) {
-        // Либо одна из пяти стандартных операций (и на последней мы остановимся)
-        // либо дефолтный кейс
-        case opPlus:
-        case opMinus:
-        case opMultipy:
-        case opDivide:
-        case opAvg:
-            calc.operations = [NSNumber numberWithInteger:menu_item];
-            break;
-        default: {
-            selectCalculatorOperation(calc);
-        }
-        
-    }
-}
-
-void loadNumbers (Calculator *calc) {
-    int first_entered = 0;
-    int second_entered = 0;
-    
-    printf("\nPlease enter two numbers and choose what did you want to do.\n");
-    printf("First one (or -1 to exit): ");
-    scanf("%i", &first_entered);
-    
-    if (first_entered > -1) {
-        printf("Second one (or -1 to exit): ");
-        scanf("%i", &second_entered);
-        
-        // Инициализируем калькулятор введенными данными
-        [calc initWithNumbersFirst:[NSNumber numberWithInt:first_entered] Second:[NSNumber numberWithInt:second_entered]];
-    }
-}
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        Calculator *calc = [Calculator new];
-        loadNumbers(calc);
+        // insert code here...
+        printf("Type some text, or -1 to write them on disk:");
+        
+        char c[255];
+        BOOL doSmthn = true;
+        
+        // Введенная строка в виде NSString
+        NSMutableString *string = [[NSMutableString alloc] init];
+                
+        // Здесь будем хранить все что пользователь ввел
+        NSMutableArray *users_input = [[NSMutableArray alloc] init];
+        
+        // Получаем пользовательский ввод
+        do {
+            // Читаем строку
+            scanf(" %s", c);
+            
+            // Переводим в NSString
+            string = [NSMutableString stringWithCString:c encoding:1];
+            
+            // Ввели -1
+            if ([string  isNotEqualTo: @"-1"] == YES) {
+                [users_input addObject:string];
+                printf("some text or -1 to write: ");
+            } else {
+                doSmthn = false;
+            }
+        } while (doSmthn);
+        
+        // Сохраняем в виде plist
+        NSString *path = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
+        path = [path stringByAppendingString:@"/UsersInput.plist"];
+        [users_input writeToFile:path atomically:YES];
+        NSLog(@"%@", users_input);
+        
+        users_input = nil;
+        NSLog(@"%@", users_input);
+        
+        users_input = [NSMutableArray arrayWithContentsOfFile:path];
+        NSLog(@"%@", users_input);
+        
     }
-    
     return 0;
 }
